707988da7a8b9df8c043f6b3a7301906a349695c
洋洋 <757955886@qq.com>
2020-07-02 17:40:44 -0700

    add reset button in high score screen, and need to implement

--- app/src/main/java/cmpt276/project/HighScoreActivity.java
+++ app/src/main/java/cmpt276/project/HighScoreActivity.java
@@ -3,6 +3,8 @@ package cmpt276.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HighScoreActivity extends AppCompatActivity {
@@ -13,6 +15,17 @@ public class HighScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_high_score);
         
         setUpHighScore();
+
+        Button btReset = findViewById(R.id.ResetButton);
+        btReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Implement reset high scores
+
+                //for (int i = 0; i < 5; i++){
+                //time[i] = "" (0.0) ; name[i] = ""; month[i] = ""; day[i] = 0; year[i] = 0; }
+            }
+        });
     }
 
     private void setUpHighScore() {
