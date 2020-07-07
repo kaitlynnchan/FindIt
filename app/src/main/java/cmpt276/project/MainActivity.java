7379267998289756e8dbdb14d2e3e5ad715f737e
Lakshay <lakshay.sethi40@gmail.com>
2020-07-07 19:20:23 -0400

    Option screen done.
    Created the empty buttons , linkage empty.

--- app/src/main/java/cmpt276/project/MainActivity.java
+++ app/src/main/java/cmpt276/project/MainActivity.java
@@ -2,19 +2,12 @@ package cmpt276.project;
 
 import android.content.Intent;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -61,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         btnOptions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = MainActivity2.makeIntent(MainActivity.this);
+                Intent intent = OptionActivity.makeIntent(MainActivity.this);
                 startActivity(intent);
             }
         });
